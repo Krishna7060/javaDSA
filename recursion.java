a97f41ad3270a1738f82b0bc699493b3ba9f2184
Krishna Nand <k.nautiyalkrishna70@gmail.com>
2024-08-12 21:54:59 +0530

    fibb series

--- recursion.java
+++ recursion.java
@@ -51,9 +51,36 @@ public class recursion {
 
 
 
-    public static void main(String[] args) {
+// public static void printFib(int a, int b ,int n){
+//     if(n==0){
+//         return;
+//     }
+//     int c = a+b;
+//     System.out.println(c);
+//     printFib(b, c, n-1);
+
+// }
+//     public static void main(String[] args) {
+//         int n=10;
+//         int a =0;
+//         int b =1;
+//         System.out.println(a);
+//         System.out.println(b);
+//         printFib(a, b, n-2);
+
+
+        
+
+        
        
         
 
-    }
-}
+//     }
+// }
+
+
+public static void main(String[] args) {
+
+    
+    
+}}
