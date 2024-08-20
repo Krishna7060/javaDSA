6ca731c268e33f8b83cf4c9cd9cc52d67473d14a
Krishna Nand <k.nautiyalkrishna70@gmail.com>
2024-08-20 18:15:42 +0530

    calculate xtoPowerN

--- recursion.java
+++ recursion.java
@@ -1,86 +1,21 @@
 public class recursion {
-    // print nunbers 1 to 5
-    // public static void printNum(int n){
-    // if(n==6){
-    // return;
-    // } else{
-    // System.out.println(n);
-    // printNum(n+1);
-    // }
-    // }
 
-    // print the sum of first natural number
-    // public static void printSum(int i, int sum, int n) {
+    public static int calculatePower(int x, int n){
+        if(n==0){
+            return 1;
+        }
 
-    //     if (i == n) {
-    //         sum += i;
-    //         System.out.println(sum);
-    //     }
+        if(x==0){
+            return 0;
+        }
 
-    //     else {
-    //         sum += i;
+       int nm1= calculatePower(x, n-1);
+       int xpN =nm1*x;
+       return xpN;
+    }
 
-    //         printSum(i + 1, sum, n);
-    //     }
-
-    // }
-
-
-
-    // print factrion of a number
-    
-//   public static int printFact(int n){
-//     if(n==0 || n==1){
-//         return 1;
-//     }
-    
-
-//      int factorel = printFact(n-1);
-//      int nfc = factorel*n;
-//      return nfc;
-
-     
-     
-    
-    
-    
-
-// }
-
-
-
-
-
-// public static void printFib(int a, int b ,int n){
-//     if(n==0){
-//         return;
-//     }
-//     int c = a+b;
-//     System.out.println(c);
-//     printFib(b, c, n-1);
-
-// }
-//     public static void main(String[] args) {
-//         int n=10;
-//         int a =0;
-//         int b =1;
-//         System.out.println(a);
-//         System.out.println(b);
-//         printFib(a, b, n-2);
-
-
-        
-
-        
-       
-        
-
-//     }
-// }
-
-
-public static void main(String[] args) {
-
-    
-    
-}}
+    public static void main(String[] args) {
+       int ans= calculatePower(2, 5);
+       System.out.println(ans);
+    }
+}
