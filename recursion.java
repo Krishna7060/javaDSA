b434e6d56e277ecc28aae267824cb752fea33a19
Krishna Nand <k.nautiyalkrishna70@gmail.com>
2024-08-26 09:50:07 +0530

    reverse a string

--- recursion.java
+++ recursion.java
@@ -1,39 +1,24 @@
 public class recursion {
-
-    // public static void towerOfHonoi( int n,String src, String Helper,String Destination){
-    //     if(n==1){
-    //         System.out.println(n+"disk transpered" +src+ "to" +Destination);
-    //         return;
-
-    //     }
+     
+     public static void printReverse(String str , int index ){
         
-    //     towerOfHonoi(n-1, src, Destination, Helper);
-    //     System.out.println(n+"disk transpered" +src+ "to" +Destination);
-    //     towerOfHonoi(n-1, Helper, src, Destination);
+          if(index==0){
+            System.out.println(str.charAt(index));
+          }  
     
-        
-    // }
-
-
-    // reverse a string
     
-    // public static void reverseString(String str, int index){
+     System.out.print(str.charAt(index));
+     printReverse(str, index-1);
 
-    //     if(index==0){
-    //         System.out.println(str.charAt(index));
-        
-    //         return;
-    //     }
-    //       System.out.print(str.charAt(index));
-    //     reverseString(str, index-1);
-        
-    // }
+     }
 
     public static void main(String[] args) {
+
+        String str ="krishna";
+        int index =str.length()-1;
+        printReverse(str, index);
         
-        String str = "krishna";
-        int index = str.length()-1;
-    reverseString(str, index);
+
 
         
     
